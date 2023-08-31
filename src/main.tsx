a61a11d472e558b7bffcab8230b45b375d754465
Andrew Stone <adrwstn@gmail.com>
2023-08-31 17:06:00 -0230

    use ReactDOM.render() instead of .createRoot() to force downgrade to R17

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/* ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+ */
